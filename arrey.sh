7391221819a48080477e6c5db7e7eaef7bc44778
Kontetsu.DevOps <erget.kabaj@gmail.com>
2021-09-12 19:19:44 +0200

    update of script

--- arrey.sh
+++ arrey.sh
@@ -3,3 +3,17 @@
 arr=(hello world)
 
 echo ${arr[@]}
+
+
+counter=o
+for ARG in {1..$loops};
+do
+if [ $counter -le 5 ];
+then
+    echo $ARG
+fi
+
+counter=$((counter+1))
+done
+
+echo times: $counter
